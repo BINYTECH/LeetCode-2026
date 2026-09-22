2f4fb8771a95389cf1dceac3021943142510f5b9
BINAY TOPNO <39939885+BINYTECH@users.noreply.github.com>
2026-09-22 23:20:11 +0530

    Time: 0 ms (100%), Space: 47.5 MB (84.14%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double myPow(double x, int n) {
+        return (double) Math.pow(x,n);
+        // double ans;
+        // if(n<0){
+        //     n= n * -1;
+        //     ans= (1/ Math.pow(x,n));
+        //     return ans;
+        // }  
+        // ans=Math.pow(x,n);
+        // return ans;
+    }
+}
